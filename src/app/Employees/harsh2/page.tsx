4927fab2627007abcba2d9a8a4adb2683fcee14f
HarshForreal <hp673315@gmail.com>
2024-04-01 19:16:16 +0530

    tried something

---
+++ src/app/Employees/harsh2/page.tsx
@@ -0,0 +1,7 @@
+export default function harsh2(){
+    return(
+        <h1>
+            harsh2 folder
+        </h1>
+    )
+}
---
+++ src/app/Employees/page.tsx
@@ -0,0 +1,3 @@
+export default function employees() {
+  return <h1 className="text-3xl text-center pt-4">Harsh</h1>;
+}
